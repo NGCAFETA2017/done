1e4037f7498068641fd61126068b552ac22775ae
Rasbpi <noe115@csu.fullerton.edu>
2016-01-09 10:22:23 +0000

    final

--- api-test.py
+++ api-test.py
@@ -1,11 +1,29 @@
+#from twilio.rest import TwilioRestClient
+
+#ACCOUNT_SID = 'AC639b11fb2dbbe5e6416a104a51d55c07'
+#TOKEN = '3817c55f21fc7c3bd075d061be13c7fd'
+
+#client = TwilioRestClient(ACCOUNT_SID, TOKEN)
+
+#client.messages.create(
+#	to= "16572066509",
+#	from_="+19513392208",
+#	body="thisisarequest",
+#)
+
+
+
+
+
+
 import requests
 
 BASE = 'https://api.twilio.com/'
 VERSION = '2010-04-01/'
 API = BASE + VERSION
 
-ACCOUNT_SID = 'AC639b11fb2dbbe5e6416a104a51d55c07'
-TOKEN = '3817c55f21fc7c3bd075d061be13c7fd'
+ACCOUNT_SID = "AC639b11fb2dbbe5e6416a104a51d55c07"
+TOKEN = "3817c55f21fc7c3bd075d061be13c7fd"
 NUMBER = '+19513392208'
 
 def sendMessage(to_number, body):
@@ -18,7 +36,7 @@ def sendMessage(to_number, body):
 		'Body': body
 		}
 
-	r = requests.post(URL, data = payload, auth = (ACCOUNT_SID,TOKEN))
+	r = requests.post(URL, data=payload, auth=(ACCOUNT_SID, TOKEN))
 	print(r)
 
 sendMessage('+16572066509', 'Hello, from diy iot')
